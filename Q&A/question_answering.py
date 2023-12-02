40d024d7abc35e1de2dea3893f29b10036ba338f
SaradhaSarah <95039756+SaradhaSarah@users.noreply.github.com>
2023-12-02 20:32:26 +0530

    Add files via upload

---
+++ Q&A/question_answering.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from transformers import pipeline, AutoTokenizer
+
+def run_question_answering(question, context):
+    model_name = "deepset/roberta-base-squad2"
+    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    result = nlp({'question': question, 'context': context})
+    return result
+
+def main():
+    st.title("Question Answering with Streamlit")
+    
+    # User input for question and context
+    question = st.text_input("Enter your question:")
+    context = st.text_area("Enter the context:")
+    
+    if st.button("Get Answer"):
+        if question and context:
+            st.write("Answer:")
+            result = run_question_answering(question, context)
+            st.write(result['answer'])
+        else:
+            st.warning("Please provide both a question and context.")
+
+if __name__ == "__main__":
+    main()
